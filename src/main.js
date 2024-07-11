57ff663f37f745eee0060f3d4c5cd27c1cbc34b8
RKYSHLM <104001439+RKYSHLM@users.noreply.github.com>
2024-07-11 19:37:53 +0800

    Update main.js
    test code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+// import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
